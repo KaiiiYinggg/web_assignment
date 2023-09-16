ca0f273609a43f9deb346119fd75525afa334ab3
KaiiiYinggg <145221377+KaiiiYinggg@users.noreply.github.com>
2023-09-17 01:43:58 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+/* =============================================
+	author: Lim kah chun
+	file :main.js
+     js for file: hb_faq.html
+   =============================================
+*/
+
+JAVASCRIPT
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
